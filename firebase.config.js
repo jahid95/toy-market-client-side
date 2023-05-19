2b2d58c68be7662a6312077a505eaee4faebbe69
Anwar <anwarjahid449@gmail.com>
2023-05-19 22:52:10 +0600

    setup envlocal

--- firebase.config.js
+++ firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFOJ2AYpR42-AieakQJD-EVnVxaCJkoZA",
-  authDomain: "kids-cars-assingment.firebaseapp.com",
-  projectId: "kids-cars-assingment",
-  storageBucket: "kids-cars-assingment.appspot.com",
-  messagingSenderId: "606517703457",
-  appId: "1:606517703457:web:3eebe2604f4c07cfb3c460"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
